1b6b1f55c67fd5d4d0e59987ef7812a26f09c367
Rafael Brandão <raffaelbrandao@gmail.com>
2019-02-10 15:46:17 -0300

    no message

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequestMapping({"/oportunidades"})
 public class OportunidadeController
